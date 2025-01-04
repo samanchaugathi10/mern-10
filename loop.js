6f7c499f7bad41ca9f05e467a84c080b6933ee12
Saman <chaugathisaman10@gmail.com>
2025-01-04 15:04:56 +0545

    formatting done

--- loop.js
+++ loop.js
@@ -1,16 +1,16 @@
-let i=11;
+let i = 11;
 do {
-   console.log(i);
-   i++ 
-} while (i<=15);
+  console.log(i);
+  i++;
+} while (i <= 15);
 
-let j=1;
-while(j<=5){
-    console.log(j);
-    j++
+let j = 1;
+while (j <= 5) {
+  console.log(j);
+  j++;
 }
 
-for(j=1;j<=5;j++){
-    console.log(j);
+for (j = 1; j <= 5; j++) {
+  console.log(j);
 }
 // new comment
--- operator.js
+++ operator.js
@@ -1,53 +1,49 @@
-let h=10;
+let h = 10;
 h++;
-console.log(h)
+console.log(h);
 
+console.log(++h);
 
-console.log(++h)
-
-let name='saman'
-let last='chauguthi'
+let name = "saman";
+let last = "chauguthi";
 name += last;
-console.log(name)
+console.log(name);
 
+let a = 10;
+let b = 11;
+console.log(a == b);
 
-let a=10
-let b=11
-console.log(a==b)
+console.log(a == b && a != b);
 
-console.log(((a==b)) && ((a!=b)))
+let user = {
+  name: "username",
+  age: 18,
+};
+console.log(user.age <= 16 ? "child" : "adult");
 
-let user={
-name:'username',
-age:18
-}
-console.log((user.age <= 16)?'child':'adult')
+let { name1, age } = user;
+console.log(age);
 
-let{name1,age}=user
-console.log(age)
+let obj1 = {
+  name: "saman",
+  address: "bhaktapur",
+  email: "xyz@email.com",
+  phone: 9840414870,
+  language: "c,php",
+};
 
+let obj2 = obj1;
 
-let obj1={
-name:'saman',
-address:'bhaktapur',
-email:'xyz@email.com',
-phone:9840414870,
-language:'c,php'
-}
+obj2.name = "new";
 
-let obj2=obj1;
+console.log(obj2.name);
 
-obj2.name='new';
+let { name: name2, email, ...obj3 } = obj1;
 
-console.log(obj2.name)
-
-let{name:name2,email,...obj3}=obj1;
-
-console.log(obj3.language)
+console.log(obj3.language);
 console.log(obj3.phone);
 
-
-let username='saman'
-let labno=10
-let emailtemp='Dear ${username},your session is at ${labno}"
-console.log(emailtemp)
+let username = "saman";
+let labno = 10;
+let emailtemp = "Dear ${username},your session is at ${labno}";
+console.log(emailtemp);
